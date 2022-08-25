39ae08cf5adf6c10997abdc88978f742b5b7cd38
Olexiy-repin <repinlyoha@gmail.com>
2022-08-25 14:08:01 +0300

    preparation for class work

---
+++ js/function-methods.js
@@ -0,0 +1,107 @@
+/*
+ *  Методи функцій call та apply
+ */
+
+//* Функції це об'єкти зі своїми властивостями та методами
+
+//* call та apply
+
+//? Викличте функцію showFullName у контексті об'єкта user
+
+// const showFullName = function (message) {
+//   console.log(`${message} ${this.firstName} ${this.lastName}`);
+// };
+
+// const user = {
+//   firstName: 'Ernest',
+//   lastName: 'Vasquez',
+//   age: 30,
+// };
+
+//? Викличте функцію showFullName у контексті об'єкта anotherUser
+// const anotherUser = {
+//   firstName: 'Lottie',
+//   lastName: 'Burgess',
+//   age: 40,
+// };
+
+//* bind
+// const showFullName = function () {
+//   console.log(`${this.firstName} ${this.lastName}`);
+// };
+
+// const user = {
+//   firstName: 'Ernest',
+//   lastName: 'Vasquez',
+//   age: 30,
+// };
+
+//* Метод об'єкта у ролі колбека
+
+// const user = {
+//   name: 'Luis',
+//   age: 30,
+
+//   showThis() {
+//     console.log(`this ---->`, this);
+//   },
+
+//   showName() {
+//     console.log(`this name is: ${this.name}`);
+//   },
+// };
+
+// const someFunction = function (callback) {
+//   callback();
+// };
+
+// someFunction(user.showName(user));
+
+/*
+? Що виведе код?
+*/
+
+// const f = function () {
+//   console.log(this.name);
+// };
+
+// const user = {
+//   name: 'Вася',
+// };
+
+// const anotherUser = {
+//   name: 'Петя',
+// };
+
+// const copyFunc = f.bind(user).bind(anotherUser);
+
+// copyFunc();
+
+/*
+? Виклик checkPassword() у наведеному нижче коді повинен перевірити пароль та викликати user.loginOk/loginFail залежно від відповіді.
+? Однак його виклик призводить до помилки. Чому?
+ */
+
+// const checkPassword = function (ok, fail) {
+//   const password = 'rockstar';
+
+//   if (password === 'rockstar') {
+//     ok();
+//   } else {
+//     fail();
+//   }
+// };
+
+// const user = {
+//   name: 'Вася',
+
+//   loginOk() {
+//     console.log(`${this.name} logged in`);
+//   },
+
+//   loginFail() {
+//     console.log(`${this.name} failed to log in`);
+//   },
+// };
+
+// checkPassword(user.loginOk(user), user.loginFail(user));
