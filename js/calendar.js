3b529d345c57b55286c9466bd42713ab2e25229b
Olexiy-repin <repinlyoha@gmail.com>
2022-09-02 12:29:14 +0300

    classwork

--- js/calendar.js
+++ js/calendar.js
@@ -6,16 +6,19 @@ const onCalendarItemClick = event => {
   // console.log('event.currentTarget: ', event.currentTarget);
   const { target } = event;
 
+  // Якщо наш цільовий елемент це не TD або порожній TD, нічого робити не потрібно
   if (target.nodeName !== 'TD' || target.textContent === '') {
     return;
   }
 
+  // Якщо це той самий елемент, перемикаємо клас active і виходимо
   if (prevCalendarItem === target) {
     target.classList.toggle('active');
 
     return;
   }
 
+  // Якщо це не перший клік, прибираємо з попереднього клас active
   if (prevCalendarItem !== null) {
     prevCalendarItem.classList.remove('active');
   }
