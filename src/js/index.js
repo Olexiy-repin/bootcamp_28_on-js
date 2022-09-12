0f20a74aaac8e34a3ba6d1b2c967c430cb68651f
Olexiy-repin <repinlyoha@gmail.com>
2022-09-12 13:02:12 +0300

    classwork

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,43 @@
-import logoImg from '../images/logo.png';
+// http://latentflip.com/loupe/?code=Y29uc29sZS5sb2coJ2ZpcnN0Jyk7CgpzZXRUaW1lb3V0KGZ1bmN0aW9uKCkgewogICAgY29uc29sZS5sb2coJ2lubmVyIHNldFRpbWVvdXQnKTsKfSwgMTAwMCk7Cgpjb25zb2xlLmxvZygnc2Vjb25kJyk7!!!
 
-console.log(logoImg);
+// console.log('Start');
 
-const logoEl = document.querySelector('.js-logo');
+// setTimeout(() => {
+//   console.log('SetTimeout');
+// }, 0);
 
-const logoImeEl = document.createElement('img');
-logoImeEl.src = `${logoImg}`;
-logoImeEl.alt = 'logotype';
+// const promise = new Promise((resolve, reject) => {
+//   console.log('In constructor Promise');
 
-logoEl.append(logoImeEl);
+//   setTimeout(() => {
+//     resolve('in then');
+//   }, 1000);
+// });
+
+// promise.then(response => {
+//   console.log(response);
+// });
+
+// console.log('End');
+
+const counterEl = document.querySelector('.js-counter');
+
+// for (let i = 0; i <= 1000; i += 1) {
+//   counterEl.innerHTML = i;
+// }
+
+let i = 0;
+
+const count = () => {
+  i += 10;
+
+  counterEl.innerHTML = i;
+
+  if (i !== 1000) {
+    setTimeout(() => {
+      count();
+    }, 0);
+  }
+};
+
+count();
